8b81605fceabe7b422ba0df1b0d58f41ee2ac6af
Mike Zamansky <zamansky@gmail.com>
2019-09-11 10:47:01 -0400

    added binary conversion map. Next is base64 map

--- main.clj
+++ main.clj
@@ -1,3 +1,28 @@
 (ns main)
 
+(def input-string "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+
+(def result-string "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+
+
+
+
+
+(def hexlookup {"0" "0000"
+                "1" "0001"
+                "2" "0010"
+                "3" "0011"
+                "4" "0100"
+                "5" "0101"
+                "6" "0110"
+                "7" "0111"
+                "8" "1000"
+                "9" "1001"
+                "a" "1010"
+                "b" "1011"
+                "c" "1100"
+                "d" "1101"
+                "e" "1110"
+                "f" "1111"})
+
 (defn -main [] (print "HELLO"))
