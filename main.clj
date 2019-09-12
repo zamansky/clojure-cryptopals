7104b06652b9c89816d061ce4e35fb18962e9f44
Mike Zamansky <zamansky@gmail.com>
2019-09-12 07:29:48 -0400

    cleaned up code a bit

--- main.clj
+++ main.clj
@@ -6,22 +6,9 @@
 (def result-string "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
 
 
-(def hexlookup {:0 "0000"
-                :1 "0001"
-                :2 "0010"
-                :3 "0011"
-                :4 "0100"
-                :5 "0101"
-                :6 "0110"
-                :7 "0111"
-                :8 "1000"
-                :9 "1001"
-                :a "1010"
-                :b "1011"
-                :c "1100"
-                :d "1101"
-                :e "1110"
-                :f "1111"})
+(def hexlookup {:0 "0000" :1 "0001" :2 "0010" :3 "0011" :4 "0100" :5 "0101" :6 "0110"
+                :7 "0111" :8 "1000" :9 "1001" :a "1010" :b "1011":c "1100" :d "1101"
+                :e "1110" :f "1111"})
 
 (def base64-keys (->> (for [x (range 64)]
                         (Integer/toString x 2))
@@ -36,21 +23,21 @@
 (def base64-map (zipmap base64-keys base64-values))
 
 (defn hex->binary [s]
+  "converts a hex string to a list of strings of binary"
   (map #( (keyword (str %)) hexlookup) s))
 
-
-
 (defn binary->base64 [s]
+  "converts a list of 6 bit binary strings to a list of base64 chars"
   (map (fn [x] ( (keyword x) base64-map) ) s))
 
 (defn encode-base64 [s]
-(let [binary-string (str/join ""(-> input-string
-                                    hex->binary))
-sixes (map #(apply str %) (partition 6 binary-string))
-result (str/join "" (-> sixes binary->base64))
-]
-result
-))
+  (let [binary-string (str/join ""(-> input-string
+                                      hex->binary))
+        sixes (map #(apply str %) (partition 6 binary-string))
+        result (str/join "" (-> sixes binary->base64))
+        ]
+    result
+    ))
 
 
 
